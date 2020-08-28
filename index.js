b5de95536ebadcb85723274d4baa4013b11acaac
KimJongwan2 <kjw900901@gmail.com>
2020-08-28 21:53:18 +0900

    :heavy_plus_sign::hand: :flags: 미들웨어 설치 및 sequelize 설치

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const session = require("express-session")
+const bodyParser = require("body-parser")
+const cors = require("cors");
 const app = express();
 const PORT = 5000;
+
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/", (req, res) => {
     res.send("Welcome to Man's Club");
