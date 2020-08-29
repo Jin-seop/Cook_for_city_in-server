2b6aead1d6f2b96a11a1863e0a400bac6cf09cef
KimJongwan2 <kjw900901@gmail.com>
2020-08-29 10:57:22 +0900

    :heavy_plus_sign: :recycle: :hand: 모델 및 마이그레이션 완료

--- migrations/20200828131209-create-favorites.js
+++ migrations/20200828131209-create-favorites.js
@@ -1,30 +1,40 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('favorites', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userid: {
-        type: Sequelize.INTEGER
-      },
-      recipeid: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    // await queryInterface.createTable('favorites', {
+    //   id: {
+    //     allowNull: false,
+    //     autoIncrement: true,
+    //     primaryKey: true,
+    //     type: Sequelize.INTEGER
+    //   },
+    //   userid: {
+    //     type: Sequelize.INTEGER,
+    //     references : { 
+    //       model: 'users', 
+    //       key: 'id'
+    //     },
+    //     onDelete : "CASCADE"
+    //   },
+    //   recipeid: {
+    //     type: Sequelize.INTEGER,
+    //     references : { 
+    //       model: 'recipe', 
+    //       key: 'id' 
+    //     },
+    //     onDelete : "CASCADE"
+    //   },
+    //   createdAt: {
+    //     allowNull: false,
+    //     type: Sequelize.DATE
+    //   },
+    //   updatedAt: {
+    //     allowNull: false,
+    //     type: Sequelize.DATE
+    //   }
+    // });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('favorites');
+    // await queryInterface.dropTable('favorites');
   }
 };
--- migrations/20200828131348-create-recipe.js
+++ migrations/20200828131348-create-recipe.js
@@ -1,39 +1,41 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('recipes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING
-      },
-      meterial: {
-        type: Sequelize.STRING
-      },
-      recipe: {
-        type: Sequelize.STRING
-      },
-      recipe_img: {
-        type: Sequelize.STRING
-      },
-      commentsid: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    // await queryInterface.createTable('recipes', {
+    //   id: {
+    //     allowNull: false,
+    //     autoIncrement: true,
+    //     primaryKey: true,
+    //     type: Sequelize.INTEGER
+    //   },
+    //   title: {
+    //     type: Sequelize.STRING
+    //   },
+    //   meterial: {
+    //     type: Sequelize.STRING
+    //   },
+    //   recipe: {
+    //     type: Sequelize.STRING
+    //   },
+    //   recipe_img: {
+    //     type: Sequelize.STRING
+    //   },
+    //   commentsid: {
+    //     type: Sequelize.INTEGER,
+    //     references : {model:'comments', key: 'id'},
+    //     onDelete : "CASCADE"
+    //   },
+    //   createdAt: {
+    //     allowNull: false,
+    //     type: Sequelize.DATE
+    //   },
+    //   updatedAt: {
+    //     allowNull: false,
+    //     type: Sequelize.DATE
+    //   }
+    // });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('recipes');
+    //await queryInterface.dropTable('recipes');
   }
 };
