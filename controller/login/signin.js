6e9f038c7313cb4893af180171033d77752c766f
KimJongwan2 <kjw900901@gmail.com>
2020-08-31 14:34:07 +0900

    :heavy_plus_sign: :soon::hand: 로그인  토큰 추가

--- controller/login/signin.js
+++ controller/login/signin.js
@@ -1,26 +1,31 @@
 const db = require("../../models");
 const crypto = require("crypto");
+const jwt = require("jsonwebtoken")
+const secretObj = require('../../config/jwt')
 
 //토큰으로 로그인 구현해야됨. => 현재는 세션으로 되어있음.
 module.exports = {
   // 로그인 요청
-  post: (req, res) => {
+  post: (req, res,next) => {
     // 로그인 요청시 입력된 비밀번호를 헤싱합니다.
     let secret1 = "쿡시인화이팅";
     const hash = crypto.createHmac("sha1", secret1);
     hash.update(req.body.password);
     let passwordHashed = hash.digest("hex");
 
-    db.Users.findOne({ where: { userId: req.body.userId } }).then(
+    let token = jwt.sign({
+      userid : req.body.userid
+    }, secretObj.secret)
+
+    db.users.findOne({ where: { userid: req.body.userid } }).then(
       (checkUser) => {
         if (checkUser) {
           if (checkUser.password === passwordHashed) {
-            // session 객체에 유저 id 추가
-            req.session.session_id = checkUser.id;
-            // 해당 session을 저장, 보내주기
-            if (req.session.session_id) {
-              res.status(201).send({ id: checkUser.id });
-            }
+            res.cookie("user", token);
+            res.json({
+              token: token
+            })
+            res.status(200).send("로그인 되었습니다.");
           } else {
             res.status(404).send("비밀번호가 틀렸습니다.");
           }
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const session = require("express-session")
 const bodyParser = require("body-parser")
 const cors = require("cors");
+const jwt = require("jsonwebtoken")
+const secretObj = require("./config/jwt")
 const app = express();
 const PORT = 5000;
 
@@ -18,6 +20,17 @@ app.use("/a", (req, res) => {
 
 app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
+//토큰 확인
+app.use("/api", function(req, res, next){
+    let token = req.headers.cookie.user;
+    let decoded = jwt.verify(token, secretObj.secret);
+    if(decoded){
+      res.send("권한이 있어서 API 수행 가능")
+    }
+    else{
+      res.send("권한이 없습니다.")
+    }
+  })
 
 
 app.listen(PORT, () => {
