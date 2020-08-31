487df0707e6929a11cda684b3571c9e0d4667516
KimJongwan2 <kjw900901@gmail.com>
2020-08-31 15:27:42 +0900

    :heavy_plus_sign: :recycle::hand: 세션 로그인 로그아웃 구현

--- controller/login/signin.js
+++ controller/login/signin.js
@@ -1,31 +1,26 @@
 const db = require("../../models");
 const crypto = require("crypto");
-const jwt = require("jsonwebtoken")
-const secretObj = require('../../config/jwt')
 
-//토큰으로 로그인 구현해야됨. => 현재는 세션으로 되어있음.
 module.exports = {
   // 로그인 요청
-  post: (req, res,next) => {
+  post: (req, res) => {
     // 로그인 요청시 입력된 비밀번호를 헤싱합니다.
+    const { userid } = req.body
     let secret1 = "쿡시인화이팅";
     const hash = crypto.createHmac("sha1", secret1);
     hash.update(req.body.password);
     let passwordHashed = hash.digest("hex");
 
-    let token = jwt.sign({
-      userid : req.body.userid
-    }, secretObj.secret)
-
-    db.users.findOne({ where: { userid: req.body.userid } }).then(
+    db.users.findOne({ where: { userid: userid } }).then(
       (checkUser) => {
         if (checkUser) {
           if (checkUser.password === passwordHashed) {
-            res.cookie("user", token);
-            res.json({
-              token: token
-            })
-            res.status(200).send("로그인 되었습니다.");
+            // session 객체에 유저 id 추가
+            req.session.session_id = checkUser.id;
+            // 해당 session을 저장, 보내주기
+            if (req.session.session_id) {
+              res.status(201).send({ id: checkUser.id });
+            }
           } else {
             res.status(404).send("비밀번호가 틀렸습니다.");
           }
