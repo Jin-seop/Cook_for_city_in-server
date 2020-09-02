38f7b821628d059e393450828acc877769dd1040
KimJongwan2 <kjw900901@gmail.com>
2020-09-02 13:36:17 +0900

    :recycle: :hand: 마이페이지 수정

--- routes/mypage.js
+++ routes/mypage.js
@@ -6,4 +6,10 @@ const { mypageController } = require("../controller");
 //회원정보 마이페이지
 router.post("/mypageGet", mypageController.mypageGet.post)
 
+//내가 쓴 댓글이 해당되는 게시글로 이동
+router.post("/mypagetocomment", mypageController.mypagetocomment.post)
+
+//즐겨찾기 해당페이지로 디동
+router.post("/mypagetofavorites", mypageController.mypagetofavorites.post)
+
 module.exports = router;
