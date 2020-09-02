72cdd3583107b10410cd1be24bd5c5470090a61a
KimJongwan2 <kjw900901@gmail.com>
2020-09-02 11:31:55 +0900

    :recycle: :hand: 회원수정 해싱해결

--- controller/signup/signup.js
+++ controller/signup/signup.js
@@ -1,15 +1,9 @@
 const db = require("../../models");
-const crypto = require("crypto");
 
 // 회원가입 요청
 module.exports = {
   post: (req, res) => {
       const {userid, password, email} = req.body;
-      //비밀번호 암호화
-      let secret1 = "쿡시인화이팅";
-      const hash = crypto.createHmac("sha1", secret1);
-      hash.update(password);
-      let passwordHashed = hash.digest("hex");  
 
     db.users.findOne({ 
       where: { 
@@ -19,7 +13,7 @@ module.exports = {
         if (!checkUserId) {
           db.users.create({
             userid: userid,
-            password: passwordHashed,
+            password: password,
             email: email,
           }).then((result) => res.status(201).send(result));
         } else {
