87915b35d1f84e1dd2b3b3da19f5e1268277db8f
KimJongwan2 <kjw900901@gmail.com>
2020-09-03 14:41:41 +0900

    :recycle: :hand: 관리자 댓글 삭제 수정

--- controller/login/signin.js
+++ controller/login/signin.js
@@ -16,8 +16,7 @@ module.exports = {
         if (checkUser) {
           if (checkUser.password === passwordHashed) {
             // session 객체에 유저 id 추가
-            req.session.session_id = checkUser.id,
-              req.session.session_userid = checkUser.userId
+            req.session.session_id = checkUser.id
             // 해당 session을 저장, 보내주기
             if (req.session.session_id) {
               res.status(201).send({ id: checkUser.id });
